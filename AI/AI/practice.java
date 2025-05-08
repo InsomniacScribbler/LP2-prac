361a885857a79011bd483350491632102c6e427c
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-05-08 16:41:46 +0530

    prims

--- AI/AI/practice.java
+++ AI/AI/practice.java
@@ -2,50 +2,52 @@ package AI;
 import java.util.*;
 
 public class practice{
+	static class Edge{
+		int vertex, weight;
+		Edge(int v, int w){
+			vertex= v;
+			weight = w;
 
-	Map<Integer, List<Integer>> graph = new HashMap<>();
-
-	public void addEdge(int u, int v){
-		graph.computeIfAbsent(u,k -> new ArrayList<>).add(v);
-		graph.computeIfAbsent(v,k-> new ArrayList<>()).add(u);
-	}
-
-	public void dfs(int node, Set<Integer> visited){
-		visited.add(node);
-		System.out.println(node);
-
-		for(int neighbour : graph.getOrDefault(node, new ArrayList<>())){
-			if(!visited.contains(neighbour)){
-				dfs(neighbour,visited);
-			}
 		}
 	}
 
-	public void startDFS(int startnode){
-		Set<Integer> visited = new HashSet<>();
-		System.out.println("DFS:");
-		dfs(startnode,visited);
-		System.out.println();
-	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int V = sc.nextInt();
+		int E = sc.nextInt();
+		List<List<Edge>> adj = new ArrayList<>();
 
-	public void startBFS(int startnode){
-		Set<Integer> visited = new HashSet<>();
-		Queue<Integer> queue = new LinkedList<>();
+		for (int i = 0; i < V; i++) {
+			adj.add(new ArrayList<>());
+		}
 
-		visited.add(startnode);
-		queue.offer(startnode);
+		for (int i = 0; i < E; i++) {
+			int u = sc.nextInt();
+			int v = sc.nextInt();
+			int w = sc.nextInt();
 
-		System.out.println("BFS traversal:");
-
-		while(!queue.isEmpty()){
-			int current = queue.poll();
-			System.out.println(current+" ");
+			adj.get(u).add(new Edge(v,w));
+			adj.get(v).add(new Edge(u,w));
+		}
 
 
-			for(int neighbour : graph.getOrDefault(current, new ArrayList<>())){
-				if(!visited.contains(neighbour)){
-					visited.add(neighbour);
-					queue.offer(neighbour);
+		boolean [] visited = new boolean[V];
+		PriorityQueue<Edge> pq = new PriorityQueue<>(Comparator.comparingInt(e -> e.weight));
+
+		pq.offer(new Edge (0,0));
+		int mstCost = 0;
+		while(!pq.isEmpty()){
+			Edge curr = pq.poll();
+			int  u = curr.vertex;
+			int w = curr.weight;
+			if(visited[u]) continue;
+			visited[u] = true;
+			mstCost+=w;
+
+			for(Edge neigbhor : adj.get(u)){
+				if(!visited[neigbhor.vertex]){
+					visited[neigbhor.vertex] = true;
+					pq.offer(new Edge(neigbhor.vertex, neigbhor.weight));
 				}
 			}
 		}
