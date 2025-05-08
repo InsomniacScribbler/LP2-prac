6ca8252a804de496fcf2f52703ee3295db122c93
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-05-08 17:00:24 +0530

    sjf

--- AI/AI/practice.java
+++ AI/AI/practice.java
@@ -1,57 +1,42 @@
 package AI;
 import java.util.*;
-
-public class practice{
-	static class Edge{
-		int vertex, weight;
-		Edge(int v, int w){
-			vertex= v;
-			weight = w;
-
-		}
+ class Processs{
+	int pid, bt;
+	Processs(int pid,int bt){
+		this.pid = pid;
+		this.bt= bt;
 	}
-
+}
+public class practice{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int V = sc.nextInt();
-		int E = sc.nextInt();
-		List<List<Edge>> adj = new ArrayList<>();
 
-		for (int i = 0; i < V; i++) {
-			adj.add(new ArrayList<>());
+		int np = sc.nextInt();
+
+		Processs[] processes = new Processs[np];
+
+		for (int i = 0; i < np; i++) {
+			int pid = sc.nextInt();
+			int bt = sc.nextInt();
+
+			processes[i] = new Processs(pid,bt);
+
 		}
 
-		for (int i = 0; i < E; i++) {
-			int u = sc.nextInt();
-			int v = sc.nextInt();
-			int w = sc.nextInt();
+		Arrays.sort(processes,Comparator.comparingInt(p->p.bt));
 
-			adj.get(u).add(new Edge(v,w));
-			adj.get(v).add(new Edge(u,w));
+		int[] wt = new int[np];
+		int[] tat = new int[np];
+
+		for(int i = 1 ; i < np ; i++){
+			wt[i] = wt[i-1]+ processes[i-1].bt;
+		}
+		for(int i = 0; i<np; i++){
+			tat[i] = processes[i].bt+ wt[i];
 		}
 
-
-		boolean [] visited = new boolean[V];
-		PriorityQueue<Edge> pq = new PriorityQueue<>(Comparator.comparingInt(e -> e.weight));
-
-		pq.offer(new Edge (0,0));
-		int mstCost = 0;
-		while(!pq.isEmpty()){
-			Edge curr = pq.poll();
-			int  u = curr.vertex;
-			int w = curr.weight;
-			if(visited[u]) continue;
-			visited[u] = true;
-			mstCost+=w;
-
-			for(Edge neigbhor : adj.get(u)){
-				if(!visited[neigbhor.vertex]){
-					visited[neigbhor.vertex] = true;
-					pq.offer(new Edge(neigbhor.vertex, neigbhor.weight));
-				}
-			}
-		}
 	}
+
 
 }
 
